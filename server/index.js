c5b214566541b780ced29ae0c45545850d323404
PortalFan202 <anibaldonoso1@gmail.com>
2022-10-05 19:44:28 +0300

    Changed default content

--- server/index.js
+++ server/index.js
@@ -5,8 +5,20 @@ app.use(cors());
 
 const port = 3001;
 
+const names = [
+    'John',
+    'Paul',
+    'George',
+    'Ringo'
+]
+
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello World!' });
+    res.status(200).json({names});
+});
+
+app.post('/new', (req, res) => {
+    names.push(req.body);
+    res.status(200).json(req.body);
 });
 
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [newName, setNewName] = useState('');
+
+  const URL = 'http://localhost:3001/';
+
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({name : newName});
+    axios.post(URL + 'new', json, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      setPeople(people => [...people, response.data]);
+      setNewName('');
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  useEffect(() => {
+    axios.get(URL)
+      .then(res => {
+        console.log(res.data)
+        setPeople(res.data.names);
+      }).catch(err => {
+        console.log(err);
+      });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>Calling Node Routes Demo</h3>
+      <form onSubmit={save}>
+        <input type="text" value={newName} onChange={e => setNewName(e.target.value)} />
+        <button>Submit</button>
+      </form>
+      <ul>
+        {people.map((person, index) => {
+          return <li key={index}>{person}</li>
+        })}
+      </ul>
+    </>
   );
 }
 
